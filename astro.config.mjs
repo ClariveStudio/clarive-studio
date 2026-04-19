7d984c075d56b989a74c46d64f524c0f142303e8
pulpbit <abdulsayed56@gmail.com>
2026-04-19 15:57:59 +0530

    Fix logo, add FAQ accordion dropdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://clarivestudio.com',
   build: {
-    format: 'file'
+    format: 'file',
+    assets: 'assets'
   },
   integrations: [],
   server: {
